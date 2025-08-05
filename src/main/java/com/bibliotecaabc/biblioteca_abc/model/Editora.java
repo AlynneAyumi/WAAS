dad653c9d74d97dc7d45a30495d20177992add69
AlynneAyumi <alynneak@outlook.com>
2025-08-05 19:05:53 -0300

    Correção Merge nos Models Autor e Editora

--- src/main/java/com/bibliotecaabc/biblioteca_abc/model/Editora.java
+++ src/main/java/com/bibliotecaabc/biblioteca_abc/model/Editora.java
@@ -1,6 +1,5 @@
 package com.bibliotecaabc.biblioteca_abc.model;
 
-<<<<<<< HEAD
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,21 +8,6 @@ import lombok.Setter;
 public class Editora {
 
     private Integer id;
-=======
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
-
-@Entity
-@Data
-public class Editora {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
->>>>>>> 6ca426f5581e16ac792fb7f80072a5ead5895585
     private String nome;
     private String endereco;
     private String telefone;
